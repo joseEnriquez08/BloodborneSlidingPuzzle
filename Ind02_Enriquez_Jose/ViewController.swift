19cd95fadd8a9e2c2ab4932c12881ea2b893eb19
Jose Enriquez <joseenriquez9891@gmail.com>
2022-02-21 18:46:29 -0600

    made outlets for all the tiles

--- Ind02_Enriquez_Jose/ViewController.swift
+++ Ind02_Enriquez_Jose/ViewController.swift
@@ -9,9 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var tiles = [[UIImageView]]()
+    
+    //variables for all tiles identified by (row, column)
+    @IBOutlet weak var tile00: UIImageView!
+    @IBOutlet weak var tile01: UIImageView!
+    @IBOutlet weak var tile02: UIImageView!
+    @IBOutlet weak var tile03: UIImageView!
+    @IBOutlet weak var tile10: UIImageView!
+    @IBOutlet weak var tile11: UIImageView!
+    @IBOutlet weak var tile12: UIImageView!
+    @IBOutlet weak var tile13: UIImageView!
+    @IBOutlet weak var tile20: UIImageView!
+    @IBOutlet weak var tile21: UIImageView!
+    @IBOutlet weak var tile22: UIImageView!
+    @IBOutlet weak var tile23: UIImageView!
+    @IBOutlet weak var tile30: UIImageView!
+    @IBOutlet weak var tile31: UIImageView!
+    @IBOutlet weak var tile32: UIImageView!
+    @IBOutlet weak var tile33: UIImageView!
+    @IBOutlet weak var tile40: UIImageView!
+    @IBOutlet weak var tile41: UIImageView!
+    @IBOutlet weak var tile42: UIImageView!
+    @IBOutlet weak var tile43: UIImageView!
+   
+   
+    
+
+    
+    
+    @IBAction func shuffle(_ sender: UIButton) {
+        
+    }
+    
+    
+    
+    @IBAction func showAnswer(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       
+        
     }
 
 
